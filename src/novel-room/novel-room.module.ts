3bc8fab11856ae115f5f4041ed4570715c39a03e
Kim Tae Hoon <kingteddy0210@gmail.com>
2023-12-27 19:53:18 +0900

    #18 roomTypeEnum 컨벤션에 맞게 2차재수정

--- src/novel-room/novel-room.module.ts
+++ src/novel-room/novel-room.module.ts
@@ -1,15 +1,15 @@
+import { NovelWriterEntity } from '@app/novel-writer/entities/novel-writer.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { novelRoomType } from 'src/novel-room/entities/enum/novel-room-type.enum';
+import { novelRoomTypeEnum } from 'src/novel-room/entities/enum/novel-room-type.enum';
 import { NovelRoomEntity } from 'src/novel-room/entities/novel-room.entity';
 import { NovelRoomController } from 'src/novel-room/novel-room.controller';
 import { NovelRoomService } from 'src/novel-room/novel-room.service';
 import { userEntity } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
-import { NovelWriterEntity } from '@app/novel-writer/entities/novel-writer.entity';
 
-import { NovelWriterRepositoryImpl } from '@app/novel-writer/repository/novel-writer.repository.impl';
 import { NovelWriterRepository } from '@app/novel-writer/repository/novel-writer.repository';
+import { NovelWriterRepositoryImpl } from '@app/novel-writer/repository/novel-writer.repository.impl';
 
 @Module({
   imports: [
@@ -27,8 +27,8 @@ import { NovelWriterRepository } from '@app/novel-writer/repository/novel-writer
       useClass: NovelWriterRepositoryImpl,
     },
     {
-      provide: 'novelRoomType',
-      useValue: novelRoomType,
+      provide: 'novelRoomTypeEnum',
+      useValue: novelRoomTypeEnum,
     },
   ],
   controllers: [NovelRoomController],
