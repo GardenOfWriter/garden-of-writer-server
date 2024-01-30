cdb46ecab0be1ca677a34e3c8047f2f4f4df68c7
cafe_jun <tb25271@gmail.com>
2024-01-30 20:16:46 +0900

    Restful URL 규칙 Read me 파일에 추가
    Restful URL 규칙을 한번 정의후 맞춰가야할것 같아 Read me 파일 수정

--- README.md
+++ README.md
@@ -1,73 +1,44 @@
-<p align="center">
-  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
-</p>
+# Restful URL 규칙
 
-[circleci-image]: https://img.shields.io/circleci/build/github/nestjs/nest/master?token=abc123def456
-[circleci-url]: https://circleci.com/gh/nestjs/nest
+## 1. url은 명사 중심으로 표현
 
-  <p align="center">A progressive <a href="http://nodejs.org" target="_blank">Node.js</a> framework for building efficient and scalable server-side applications.</p>
-    <p align="center">
-<a href="https://www.npmjs.com/~nestjscore" target="_blank"><img src="https://img.shields.io/npm/v/@nestjs/core.svg" alt="NPM Version" /></a>
-<a href="https://www.npmjs.com/~nestjscore" target="_blank"><img src="https://img.shields.io/npm/l/@nestjs/core.svg" alt="Package License" /></a>
-<a href="https://www.npmjs.com/~nestjscore" target="_blank"><img src="https://img.shields.io/npm/dm/@nestjs/common.svg" alt="NPM Downloads" /></a>
-<a href="https://circleci.com/gh/nestjs/nest" target="_blank"><img src="https://img.shields.io/circleci/build/github/nestjs/nest/master" alt="CircleCI" /></a>
-<a href="https://coveralls.io/github/nestjs/nest?branch=master" target="_blank"><img src="https://coveralls.io/repos/github/nestjs/nest/badge.svg?branch=master#9" alt="Coverage" /></a>
-<a href="https://discord.gg/G7Qnnhy" target="_blank"><img src="https://img.shields.io/badge/discord-online-brightgreen.svg" alt="Discord"/></a>
-<a href="https://opencollective.com/nest#backer" target="_blank"><img src="https://opencollective.com/nest/backers/badge.svg" alt="Backers on Open Collective" /></a>
-<a href="https://opencollective.com/nest#sponsor" target="_blank"><img src="https://opencollective.com/nest/sponsors/badge.svg" alt="Sponsors on Open Collective" /></a>
-  <a href="https://paypal.me/kamilmysliwiec" target="_blank"><img src="https://img.shields.io/badge/Donate-PayPal-ff3f59.svg"/></a>
-    <a href="https://opencollective.com/nest#sponsor"  target="_blank"><img src="https://img.shields.io/badge/Support%20us-Open%20Collective-41B883.svg" alt="Support us"></a>
-  <a href="https://twitter.com/nestframework" target="_blank"><img src="https://img.shields.io/twitter/follow/nestframework.svg?style=social&label=Follow"></a>
-</p>
-  <!--[![Backers on Open Collective](https://opencollective.com/nest/backers/badge.svg)](https://opencollective.com/nest#backer)
-  [![Sponsors on Open Collective](https://opencollective.com/nest/sponsors/badge.svg)](https://opencollective.com/nest#sponsor)-->
+code1
 
-## Description
+    POST : /novel-room/create-room (X)
+    POST : /novel-room (O)
 
-[Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
+code3
 
-## Installation
+## 2. CRUD 는 HTTP method 표현
 
-```bash
-$ npm install
-```
+code1
 
-## Running the app
+    GET    : /novel-room -> Get All room
+    POST   : /novel-room -> Create new room
+    GET    : /novel-room -> Get room for given id
+    PUT    : /novel-room -> Update room for given id
+    DELETE : /novel-room -> Delete room for given id
 
-```bash
-# development
-$ npm run start
+code3
 
-# watch mode
-$ npm run start:dev
+## 3. /(slash)로 계층을 구분
 
-# production mode
-$ npm run start:prod
-```
+code1
 
-## Test
+    GET    : /novel-room/seq
+    GET    : /novel-room/writer
+    GET    : /novel-room/writer/{id}
 
-```bash
-# unit tests
-$ npm run test
+code3
 
-# e2e tests
-$ npm run test:e2e
+## 3. (\_)underbar 와 대문자 대신 (-)hyphens 및 소문자를 사용
 
-# test coverage
-$ npm run test:cov
-```
+code1
 
-## Support
+    /novel-Room (X)
+    /novel_room (X)
+    /novel-room (0)
 
-Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
+code3
 
-## Stay in touch
-
-- Author - [Kamil Myśliwiec](https://kamilmysliwiec.com)
-- Website - [https://nestjs.com](https://nestjs.com/)
-- Twitter - [@nestframework](https://twitter.com/nestframework)
-
-## License
-
-Nest is [MIT licensed](LICENSE).
+참조 : https://restfulapi.net/resource-naming/
--- src/novel-room/novel-room.service.ts
+++ src/novel-room/novel-room.service.ts
@@ -1,20 +1,18 @@
 import { ChapterEntity } from '@app/chapter/entities/chapter.entity';
 import { ChapterStatusEnum } from '@app/chapter/entities/enums/chapter-status.enum';
 import { ChapterRepositoryToken } from '@app/chapter/repository/chapter.repository';
-import { ActionsFactory } from '@app/commons/abilities/action.factory';
-import { PagingationResponse } from '@app/commons/pagination/pagination.response';
-import { CreateNovelRoomDto } from '@app/novel-room/dto/create-novel-room.dto';
-import { UpdateNovelRoomDto } from '@app/novel-room/dto/update-novel-room.dto';
-import { NovelRoomEntity } from '@app/novel-room/entities/novel-room.entity';
 import { NovelWriterCategoryEnum } from '@app/novel-writer/entities/enums/novel-writer-category.enum';
 import { NovelWriterEntity } from '@app/novel-writer/entities/novel-writer.entity';
 import {
   NovelWriterRepository,
   NovelWriterRepositoryToken,
 } from '@app/novel-writer/repository/novel-writer.repository';
-import { UserEntity } from '@app/user/entities/user.entity';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateNovelRoomDto } from 'src/novel-room/dto/create-novel-room.dto';
+import { UpdateNovelRoomDto } from 'src/novel-room/dto/update-novel-room.dto';
+import { NovelRoomEntity } from 'src/novel-room/entities/novel-room.entity';
+
 import { In, Repository } from 'typeorm';
 import { ChapterRepository } from '../chapter/repository/chapter.repository';
 import { NovelWriterStatusEnum } from '../novel-writer/entities/enums/novel-writer-status.enum';
@@ -23,6 +21,7 @@ import { FindAttendStatusNovelRoomDto } from './dto/response/find-attend-status.
 import { NovelRoomDuplicationSubTitleException } from './exceptions/duplicate-subtitle.exception';
 import { NovelRoomDuplicationTitleException } from './exceptions/duplicate-title.exception';
 import { NovelRoomNotFoundException } from './exceptions/not-found.exception';
+import { UserEntity } from '@app/user/entities/user.entity';
 
 @Injectable()
 export class NovelRoomService {
@@ -38,7 +37,6 @@ export class NovelRoomService {
     private readonly novelWriterRepository: NovelWriterRepository,
     @Inject(ChapterRepositoryToken)
     private readonly chapterRepository: ChapterRepository,
-    private readonly actionFactory: ActionsFactory,
   ) {}
 
   async getAllRooms(user: UserEntity, dto: FindAttendQueryDto): Promise<any> {
@@ -46,7 +44,7 @@ export class NovelRoomService {
      *  참여중 미참여중 필터링
      */
     const roomFilter = dto.queryConvertStatus();
-    const [rooms, totalCount] = await this.novelRoomRepository.findAndCount({
+    const rooms = await this.novelRoomRepository.find({
       relations: ['novelWriter', 'novelWriter.user'],
       where: {
         novelWriter: {
@@ -54,23 +52,21 @@ export class NovelRoomService {
           status: In(roomFilter),
         },
       },
-      take: dto.take,
-      skip: dto.skip,
     });
 
     if (!rooms || rooms.length === 0) {
       return [];
     }
-    const items = rooms.map(
+    return rooms.map(
       (room: NovelRoomEntity) => new FindAttendStatusNovelRoomDto(user, room),
     );
-    return new PagingationResponse(totalCount, dto.chuckSize, items);
   }
   async createRoom(createNovelRoomDto: CreateNovelRoomDto): Promise<void> {
     const { title, subTitle } = createNovelRoomDto;
     /**
      *  성능상 이슈 findOne 보다 exist 이 리소스 소비가 덜 사용됨
      */
+
     const checkTitle = await this.novelRoomRepository.exist({
       where: { title },
     });
@@ -128,23 +124,10 @@ export class NovelRoomService {
     return room;
   }
 
-  async deleteRoom(
-    user: UserEntity,
-    novelRoom: NovelRoomEntity,
-  ): Promise<void> {
-    const isOwner = await this.actionFactory.canDeleteRoom(user);
-
-    if (isOwner) {
-      const room = await this.novelRoomRepository.findOne({
-        where: { id: novelRoom.id },
-      });
-
-      if (!room) {
-        throw new NovelRoomNotFoundException();
-      }
-
-      await this.novelRoomRepository.delete(room.id);
-    }
+  async deleteRoom(id: number): Promise<void> {
+    await this.novelRoomRepository.delete({
+      id: id,
+    });
   }
 
   async updateRoom(
