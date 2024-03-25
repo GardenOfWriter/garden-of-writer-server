663615e3eebcdbe80615e36194c245078d0b67dd
Kim Tae Hoon <kingteddy0210@gmail.com>
2024-03-25 21:25:07 +0900

    #58 divide User interface

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { UserEntity } from '@app/user/entities/user.entity';
+import { UserRepositoryToken } from '@app/user/repository/user.repository';
+import { UserRepositoryImpl } from '@app/user/repository/user.repository.impl';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
@@ -12,7 +13,14 @@ import { UserRepository } from './user.repository';
       UserEntity, //
     ]),
   ],
-  providers: [UserService, UserRepository],
+  providers: [
+    UserService,
+    UserRepository,
+    {
+      provide: UserRepositoryToken,
+      useClass: UserRepositoryImpl,
+    },
+  ],
   exports: [UserService, UserRepository],
   controllers: [UserController],
 })
